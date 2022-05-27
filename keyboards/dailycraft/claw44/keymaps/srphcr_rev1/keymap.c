1762179426408db287aee9b9be6eec820daf2ca0
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2022-05-27 15:32:31 +0900

    claw44のキーマップ

--- keyboards/dailycraft/claw44/keymaps/srphcr_rev1/keymap.c
+++ keyboards/dailycraft/claw44/keymaps/srphcr_rev1/keymap.c
@@ -41,24 +41,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYMBOL] = LAYOUT(
-        KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_NO,    KC_NO,    KC_BSPC,
-        KC_NO,    KC_NO,    KC_GRV,   KC_LPRN,  KC_LBRC,  KC_MINS,    KC_EQL,   KC_RBRC,  KC_RPRN,  KC_QUOT,  KC_BSLS,  KC_ENT,
-        KC_LSFT,  KC_NO,    KC_TILD,  KC_LT,    KC_LCBR,  KC_UNDS,    KC_PLUS,  KC_RCBR,  KC_GT,    KC_DQUO,  KC_PIPE,  KC_RSFT,
-                            KC_LGUI,  KC_NO,    KC_LCTL,  KC_TAB,     KC_SPC,   KC_RCTL,  KC_TRNS,  KC_RALT
+        KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_NO,    KC_NO,    KC_TRNS,
+        KC_NO,    KC_NO,    KC_GRV,   KC_LPRN,  KC_LBRC,  KC_MINS,    KC_EQL,   KC_RBRC,  KC_RPRN,  KC_QUOT,  KC_BSLS,  KC_TRNS,
+        KC_TRNS,  KC_NO,    KC_TILD,  KC_LT,    KC_LCBR,  KC_UNDS,    KC_PLUS,  KC_RCBR,  KC_GT,    KC_DQUO,  KC_PIPE,  KC_TRNS,
+                            KC_TRNS,  KC_NO,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 
     [_NUMBER] = LAYOUT(
-        KC_ESC,   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_BSPC,
-        KC_NO,    KC_1,   KC_2,     KC_3,     KC_4,     KC_5,      KC_6,    KC_7,     KC_8,   KC_9,    KC_0,   KC_ENT,
-        KC_LSFT,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_RSFT,
-                          KC_LGUI,  KC_TRNS,  KC_LCTL,  KC_TAB,    KC_SPC,  KC_RCTL,  KC_NO,  KC_RALT
+        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
+        KC_NO,    KC_1,   KC_2,     KC_3,     KC_4,     KC_5,       KC_6,     KC_7,     KC_8,   KC_9,    KC_0,   KC_TRNS,
+        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
+                          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_NO,  KC_TRNS
     ),
 
     [_FUNCTION] = LAYOUT(
-        KC_ESC,   KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_BSPC,
-        KC_TRNS,  KC_F1,  KC_F2,    KC_F3,  KC_F4,    KC_F5,     KC_F6,   KC_F7,    KC_F8,  KC_F9,    KC_F10,  KC_ENT,
-        KC_LSFT,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,     KC_NO,   KC_LEFT,  KC_UP,  KC_RGHT,  KC_NO,   KC_RSFT,
-                          KC_LGUI,  KC_NO,  KC_LALT,  KC_TAB,    KC_SPC,  KC_RCTL,  KC_NO,  KC_DOWN
+        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_TRNS,
+        KC_TRNS,  KC_F1,  KC_F2,    KC_F3,  KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,  KC_F9,    KC_F10,  KC_TRNS,
+        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,      KC_NO,    KC_LEFT,  KC_UP,  KC_RGHT,  KC_NO,   KC_TRNS,
+                          KC_TRNS,  KC_NO,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_NO,  KC_DOWN
     ),
 };
 
@@ -119,7 +119,7 @@ const char *read_keylog(void) { return keylog_str; }
 const char *read_keylogs(void) { return keylogs_str; }
 
 bool oled_task_user(void) {
-    if (is_keyboard_left()) {
+    if (is_keyboard_master()) {
         render_layer_state();
         /* oled_write_ln(read_keylog(), false); */
         /* oled_write_ln(read_keylogs(), false); */
@@ -137,7 +137,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_left()) return OLED_ROTATION_180;
+    if (!is_keyboard_master()) return OLED_ROTATION_180;
     return rotation;
 }
 
