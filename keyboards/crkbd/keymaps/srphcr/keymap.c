50560ede68eeafcfc7eb4fb64b09497531d34eb2
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2022-05-29 21:32:28 +0900

    crkbdのレイヤーキーを単体にしてctrlとtab,spcを共有

--- keyboards/crkbd/keymaps/srphcr/keymap.c
+++ keyboards/crkbd/keymaps/srphcr/keymap.c
@@ -42,21 +42,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_NO,    KC_NO,    KC_TRNS,
         KC_NO,    KC_NO,    KC_GRV,   KC_LPRN,  KC_LBRC,  KC_MINS,    KC_EQL,   KC_RBRC,  KC_RPRN,  KC_QUOT,  KC_BSLS,  KC_TRNS,
         KC_TRNS,  KC_NO,    KC_TILD,  KC_LT,    KC_LCBR,  KC_UNDS,    KC_PLUS,  KC_RCBR,  KC_GT,    KC_DQUO,  KC_PIPE,  KC_TRNS,
-                                      KC_TRNS,  KC_TRNS,  KC_TAB,     KC_TRNS,  KC_TRNS,  KC_TRNS
+                                      KC_TRNS,  KC_NO,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 
     [_NUMBER] = LAYOUT(
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
-        KC_NO,    KC_1,   KC_2,     KC_3,     KC_4,     KC_5,       KC_6,    KC_7,     KC_8,   KC_9,    KC_0,   KC_TRNS,
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
-                                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_SPC,  KC_TRNS,  KC_TRNS
+        KC_TRNS,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
+        KC_NO,    KC_1,   KC_2,   KC_3,     KC_4,     KC_5,       KC_6,     KC_7,   KC_8,   KC_9,   KC_0,   KC_TRNS,
+        KC_TRNS,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_NO,  KC_TRNS
     ),
 
     [_FUNCTION] = LAYOUT(
-        KC_TRNS,  RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RESET,     KC_NO,   RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_TRNS,
-        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,
-        KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_LEFT,  KC_UP,    KC_RGHT,  KC_NO,    KC_TRNS,
-                                      KC_TRNS,  KC_TRNS,  KC_TAB,    KC_SPC,  KC_TRNS,  KC_DOWN
+        KC_TRNS,  RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RESET,      KC_NO,    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_TRNS,
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,
+        KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_LEFT,  KC_UP,    KC_RGHT,  KC_NO,    KC_TRNS,
+                                      KC_TRNS,  KC_NO,    KC_TRNS,    KC_TRNS,  KC_NO,    KC_DOWN
     ),
 };
 
