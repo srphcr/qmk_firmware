83215a752cf5a8b558ad6dcb388add49333dbd86
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2022-06-01 10:44:35 +0900

    crkbdでRETRO_TAPPING有効化

--- keyboards/crkbd/keymaps/srphcr/config.h
+++ keyboards/crkbd/keymaps/srphcr/config.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define TAPPING_TOGGLE 2
 #define PERMISSIVE_HOLD
+#define RETRO_TAPPING
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
