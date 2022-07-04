427c19ffdc358619d81dd30e3585c844db9c401c
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2022-07-04 14:39:31 +0900

    Mod Tap周りの挙動を調整(シンプルに)

--- keyboards/crkbd/keymaps/srphcr/config.h
+++ keyboards/crkbd/keymaps/srphcr/config.h
@@ -28,11 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
-#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 400
 #define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+#define TAPPING_TOGGLE 2
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
