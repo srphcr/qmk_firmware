c5370b69ae06ebdc7d007819663937c5b8caf19a
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2022-09-21 07:44:22 +0900

    結局TAP周りの挙動を元に戻す

--- keyboards/crkbd/keymaps/srphcr/config.h
+++ keyboards/crkbd/keymaps/srphcr/config.h
@@ -28,8 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TERM 400
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
 #define TAPPING_TOGGLE 2
 
 #ifdef RGBLIGHT_ENABLE
