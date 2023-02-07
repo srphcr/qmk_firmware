8a6267b6012f9adcfd4676d5fae38ee1cf8f27c7
Tomoyuki Kakuda <tomoyuki.kakuda@gmail.com>
2023-02-07 16:08:36 +0900

    master rebase & claw44とcrkbdの差分吸収

--- keyboards/dailycraft/claw44/keymaps/srphcr/keymap.c
+++ keyboards/dailycraft/claw44/keymaps/srphcr/keymap.c
@@ -30,35 +30,35 @@ enum layer_number {
 
 #define KC_MO_SYM MO(_SYMBOL)
 #define KC_MO_NUM MO(_NUMBER)
-#define KC_TT_FUN TT(_FUNCTION)
+#define KC_MO_FUN MO(_FUNCTION)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT(
         KC_ESC,     KC_Q,  KC_W,     KC_E,       KC_R,     KC_T,      KC_Y,    KC_U,     KC_I,       KC_O,    KC_P,     KC_BSPC,
-        KC_TT_FUN,  KC_A,  KC_S,     KC_D,       KC_F,     KC_G,      KC_H,    KC_J,     KC_K,       KC_L,    KC_SCLN,  KC_ENT,
+        KC_MO_FUN,  KC_A,  KC_S,     KC_D,       KC_F,     KC_G,      KC_H,    KC_J,     KC_K,       KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,    KC_Z,  KC_X,     KC_C,       KC_V,     KC_B,      KC_N,    KC_M,     KC_COMM,    KC_DOT,  KC_SLSH,  KC_RSFT,
                            KC_LGUI,  KC_MO_NUM,  KC_LCTL,  KC_TAB,    KC_SPC,  KC_RCTL,  KC_MO_SYM,  KC_RALT
     ),
 
     [_SYMBOL] = LAYOUT(
-        KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_NO,    KC_NO,    KC_TRNS,
-        KC_NO,    KC_NO,    KC_GRV,   KC_LPRN,  KC_LBRC,  KC_MINS,    KC_EQL,   KC_RBRC,  KC_RPRN,  KC_QUOT,  KC_BSLS,  KC_TRNS,
-        KC_TRNS,  KC_NO,    KC_TILD,  KC_LT,    KC_LCBR,  KC_UNDS,    KC_PLUS,  KC_RCBR,  KC_GT,    KC_DQUO,  KC_PIPE,  KC_TRNS,
-                            KC_TRNS,  KC_NO,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+        KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_NO,    KC_NO,    KC_BSPC,
+        KC_NO,    KC_NO,    KC_GRV,   KC_LPRN,  KC_LBRC,  KC_MINS,    KC_EQL,   KC_RBRC,  KC_RPRN,  KC_QUOT,  KC_BSLS,  KC_ENT,
+        KC_LSFT,  KC_NO,    KC_TILD,  KC_LT,    KC_LCBR,  KC_UNDS,    KC_PLUS,  KC_RCBR,  KC_GT,    KC_DQUO,  KC_PIPE,  KC_RSFT,
+                            KC_LGUI,  KC_NO,    KC_LCTL,  KC_TAB,     KC_SPC,   KC_RCTL,  KC_TRNS,  KC_RALT
     ),
 
     [_NUMBER] = LAYOUT(
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
-        KC_NO,    KC_1,   KC_2,     KC_3,     KC_4,     KC_5,       KC_6,     KC_7,     KC_8,   KC_9,    KC_0,   KC_TRNS,
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
-                          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_NO,  KC_TRNS
+        KC_ESC,   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_BSPC,
+        KC_NO,    KC_1,   KC_2,     KC_3,     KC_4,     KC_5,      KC_6,    KC_7,     KC_8,   KC_9,    KC_0,   KC_ENT,
+        KC_LSFT,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_RSFT,
+                          KC_LGUI,  KC_TRNS,  KC_LCTL,  KC_TAB,    KC_SPC,  KC_RCTL,  KC_NO,  KC_RALT
     ),
 
     [_FUNCTION] = LAYOUT(
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_TRNS,
-        KC_TRNS,  KC_F1,  KC_F2,    KC_F3,  KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,  KC_F9,    KC_F10,  KC_TRNS,
-        KC_TRNS,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,      KC_NO,    KC_LEFT,  KC_UP,  KC_RGHT,  KC_NO,   KC_TRNS,
-                          KC_TRNS,  KC_NO,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_NO,  KC_DOWN
+        KC_ESC,   KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,     KC_NO,   KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_BSPC,
+        KC_TRNS,  KC_F1,  KC_F2,    KC_F3,  KC_F4,    KC_F5,     KC_F6,   KC_F7,    KC_F8,  KC_F9,    KC_F10,  KC_ENT,
+        KC_LSFT,  KC_NO,  KC_NO,    KC_NO,  KC_NO,    KC_NO,     KC_NO,   KC_LEFT,  KC_UP,  KC_RGHT,  KC_NO,   KC_RSFT,
+                          KC_LGUI,  KC_NO,  KC_LCTL,  KC_TAB,    KC_SPC,  KC_RCTL,  KC_NO,  KC_DOWN
     ),
 };
 
